2a0b49802ef9e0ffc511be52401247ac75fc6a3d
kathrynleech <105227413+kathrynleech@users.noreply.github.com>
2022-07-12 11:05:57 -0400

    R Script
    My first R script.

---
+++ LASER_lab1_codealong.R
@@ -0,0 +1,15 @@
+add_numbers <- function(number_1, number_2) {
+  number_1 + number_2
+}
+
+add_numbers(5,10)
+
+library(ggplot2)
+library(readr)
+
+sci_online_classes <- read_csv("data/sci-online-classes.csv")
+View(sci_online_classes)
+sci_data <- sci_online_classes
+
+ggplot(sci_data, aes(x = FinalGradeCEMS)) +
+  geom_histogram(fill = "blue")
